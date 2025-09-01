c6e56c000058722e43416c67834e1234cd619427
kunalpehere <kunal.pehere22@vit.edu>
2025-09-01 23:48:25 +0530

    done

---
+++ vibecode-starters/hono-nodejs-starter/index.js
@@ -0,0 +1,13 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.json({ message: 'Welcome to Hono Node.js Starter!' });
+});
+
+app.get('/api/hello', (c) => {
+  return c.json({ message: 'Hello from Hono API!' });
+});
+
+export default app;
